fb99a159ab07423b9ec96acc08dd9e36564b4850
que1 <que1@163.com>
2018-05-30 00:50:06 +0800

    init

---
+++ src/main/java/com/test/spring/cloud/eureka/client/api/HelloController.java
@@ -0,0 +1,49 @@
+package com.test.spring.cloud.eureka.client.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * ProjectName: com.test.spring.cloud.eureka.client.api
+ * ClassName:   HelloController
+ * Copyright:
+ * Company:     xunlei
+ * @author:     queyiwen
+ * @version:    1.0
+ * @since:      jdk 1.7
+ * Create at:   2018/5/29
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * Date       Author      Version      Description
+ * -------------------------------------------------------------
+ *
+ *
+ */
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        List<String> serviceList = this.discoveryClient.getServices();
+        for (String serviceName : serviceList) {
+            logger.info("service-name: " + serviceName);
+        }
+        return "hello world";
+    }
+
+
+}
