19aa1483b6b6c3739849a654c46369b95ad138ac
queyiwen <queyiwen@xunlei.com>
2018-06-22 10:46:45 +0800

    add controller

--- src/main/java/com/test/spring/cloud/eureka/provider/api/TestController.java
+++ src/main/java/com/test/spring/cloud/eureka/provider/api/TestController.java
@@ -2,42 +2,36 @@ package com.test.spring.cloud.eureka.provider.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * ProjectName: com.test.spring.cloud.eureka.provider.api
- * ClassName:   NlpController
+ * ClassName:   TestController
  * Copyright:
  * Company:     xunlei
- * @author:     queyiwen
- * @version:    1.0
- * @since:      jdk 1.7
- * Create at:   2018/5/30
+ *
+ * @author: queyiwen
+ * @version: 1.0
+ * @since: jdk 1.7
+ * Create at:   2018/6/22
  * Description:
  * <p>
  * <p>
  * Modification History:
  * Date       Author      Version      Description
  * -------------------------------------------------------------
- *
- *
  */
 @RestController
-public class NlpController {
+@RequestMapping(value = "/test/api")
+public class TestController {
 
     private static final Logger logger = LoggerFactory.getLogger(NlpController.class);
-
-    @GetMapping(value = "/textfilter-get")
-    @ResponseBody
-    public String filterGet(HttpServletRequest request, HttpServletResponse response) {
-        String param1 = request.getParameter("param1");
-        String param2 = request.getParameter("param2");
-        logger.info("/textfilter-get, param1: " + param1 + ", param2: " + param2);
-        return "{\"noPass\":\"0.1813\",\"pass\":\"0.9645\"}";
-    }
 
     @PostMapping(value = "/textfilter-post")
     @ResponseBody
@@ -45,7 +39,7 @@ public class NlpController {
         String param1 = request.getParameter("param1");
         String param2 = request.getParameter("param2");
         logger.info("/textfilter-post, param1: " + param1 + ", param2: " + param2);
-        return "{\"noPass\":\"0.1813\",\"pass\":\"0.9645\"}";
+        return "{\"noPass\":\"0.111\",\"pass\":\"0.888\"}";
     }
 
 }
